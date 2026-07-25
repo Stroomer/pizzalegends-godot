6ae2806431b7db40d22d7471faa01859d1ca1b86
Lukas Stroomer <lukasstroomer@gmail.com>
2026-07-25 02:08:54 +0200

    add people animations

--- scenes/objects/person/person.gd
+++ scenes/objects/person/person.gd
@@ -7,13 +7,6 @@ var skin := "Hero":
 		skin = value
 		if is_node_ready():
 			_update_sprite();
-
-
-
-#var skin = "Hero":
-	#set(new_skin_value):
-		#skin = new_skin_value;
-		#_update_sprite();
 		
 var sprite_map = {
 	"Hero": "res://art/people/hero.png",
@@ -31,6 +24,7 @@ var sprite_map = {
 
 var is_controllable = false;
 var manual_destination_scene;
+var sprite_direction = "DOWN";
 
 func _ready() -> void:
 	manual_destination_scene = load("res://scenes/objects/destination/destination.tscn").instantiate();
@@ -66,8 +60,10 @@ func _physics_process_manual_set_destination() -> void:
 		elif dir == Constants.DIRS.DOWN:
 			print("Move Down");
 			manual_destination_scene.position = Vector2(position.x, position.y+160);
+		if dir:
+			sprite_direction = dir;	
 
-func _move_towards_destination(delta) -> void:
+func _move_towards_destination(delta:float) -> void:
 	var distance = position.distance_to(manual_destination_scene.position);
 	if distance > 3:
 		var direction = position.direction_to(manual_destination_scene.position);
@@ -76,3 +72,34 @@ func _move_towards_destination(delta) -> void:
 		move_and_slide();
 	else:
 		velocity = Vector2(0,0);
+		
+func _process(delta:float) -> void:
+	if Engine.is_editor_hint():
+		return;
+	_proces_AnimationPlayer();
+
+const ANIMATION_STAND = "STAND";
+const ANIMATION_WALK  = "WALK";
+var animation_map = {
+	ANIMATION_STAND: {
+		Constants.DIRS.LEFT:  "Stand_Left",
+		Constants.DIRS.RIGHT: "Stand_Right",
+		Constants.DIRS.UP:    "Stand_Up",
+		Constants.DIRS.DOWN:  "Stand_Down",
+	},
+	ANIMATION_WALK: {
+		Constants.DIRS.LEFT:  "Walk_Left",
+		Constants.DIRS.RIGHT: "Walk_Right",
+		Constants.DIRS.UP:    "Walk_Up",
+		Constants.DIRS.DOWN:  "Walk_Down",
+	},
+};
+
+
+func _proces_AnimationPlayer() -> void:
+	var animation_type = ANIMATION_STAND;
+	if velocity != Vector2.ZERO:
+		animation_type = ANIMATION_WALK;
+	
+	var animation_to_play = animation_map[animation_type][sprite_direction];
+	$AnimationPlayer.play(animation_to_play);
