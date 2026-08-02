579a08bb74d254cb6d645fd90892902fa1e22cb2
Lukas Stroomer <lukasstroomer@gmail.com>
2026-08-02 16:22:36 +0200

    add map fade transitions

--- scenes/main.gd
+++ scenes/main.gd
@@ -7,10 +7,10 @@ func _ready() -> void:
 	# Subscribe to cutscene manager group
 	add_to_group("cutscene_receivers");
 	
-	_start_map("map_DemoRoom", null);  # map_DemoRoom map_City
+	_start_map("map_DemoRoom", null, Constants.DIRS.DOWN);  # map_DemoRoom map_City
 
 
-func _start_map(map_name, hero_start_position=null) -> void:
+func _start_map(map_name, hero_start_position=null, hero_direction=Constants.DIRS.DOWN) -> void:
 	
 	var map_scene_path = "res://scenes/maps/{map_name}.tscn".format({"map_name": map_name});
 	loaded_map = load(map_scene_path).instantiate();
@@ -20,6 +20,7 @@ func _start_map(map_name, hero_start_position=null) -> void:
 	var hero = load("res://scenes/objects/person/person.tscn").instantiate();
 	hero.name = "HERO";
 	hero.is_controllable = true;
+	hero.sprite_direction = hero_direction;
 	
 	var hero_spawn = loaded_map.get_node("hero_spawn");
 	hero_spawn.visible = false;
@@ -74,6 +75,17 @@ func cutscene_lookup_requested(body)->void:
 		start_cutscene(events);
 
 func cutscene_map_change_requested(go_to_map, go_to_position)->void:
+	# Fade in the fader
+	var hero_direction = get_object_by_name("HERO").sprite_direction;
+	var fader = $CanvasLayer/screen_fade;
+	fader.fade_in();
+	await fader.faded_in;
+	
+	# Change the map
 	camera.queue_free();
 	loaded_map.queue_free();
-	_start_map(go_to_map, go_to_position);
+	_start_map(go_to_map, go_to_position, hero_direction);
+	
+	await get_tree().create_timer(0.2).timeout;
+	fader.fade_out();
+	await fader.faded_out;
